b28de0af98d1bf3abcb6a3f4bfc4bf3ca5d50ac4
Kyle1668 <kyledevinobrien1>
2016-12-03 00:58:14 -0800

    Syntax Fixes

--- clock.js
+++ clock.js
@@ -1,18 +1,15 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
 
     // Checks if input is an invalid time measurment. EX 5:96 or 56:35
     function checkInput(hour, minute) {
-        if (isNaN(hour) || isNaN(minute)) {   //Checks if the input is a number.
+        if (isNaN(hour) || isNaN(minute)) {    //Checks if the input is a number.
             $("#result").val("Invalid Time");
-        }
-        else if (hour > 12 || hour < 1) {    // Checks for invalid hour.
+        } else if (hour > 12 || hour < 1) {    // Checks for invalid hour.
             $("#result").val("Invalid Time");
-        }
-        else if (minute > 59 || minute < 0) {   // Checks for invalid minute.
+        } else if (minute > 59 || minute < 0) {    // Checks for invalid minute.
             $("#result").val("Invalid Time");
-        }
-        else {  // If valid input.
+        } else {    // If valid input.
             return true;
         }
     };
@@ -20,17 +17,16 @@ $(document).ready(function () {
 
     // Displays the calculated angle in the results box.
     function printAngle(hour, minute, angle) {
-        if ( angle >= 360) {
-            $("#result").val(Math.abs(Number(360 - angle)) + "°");   // Prints Answer
-        }
-        else {
-            $("#result").val(Math.abs(Number(angle)) + "°");   // Prints Answer
+        if (angle >= 360) {
+            $("#result").val(Math.abs(Number(360 - angle)) + "°");    // Prints Answer
+        } else {
+            $("#result").val(Math.abs(Number(angle)) + "°");    // Prints Answer
         }
     };
 
 
     // Runs on the Calculate button click event.
-    $(".btn").click(function () {
+    $(".btn").click(function() {
 
         var hour = $("#hour").val();
         var minute = $("#minute").val();
